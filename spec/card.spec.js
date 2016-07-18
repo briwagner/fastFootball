aa526b110485531550a9b82aca71f9ecb8078036
Brian Wagner <briwagner@outlook.com>
2016-07-18 12:51:09 -0400

    clean up language in card spec to match others

--- spec/card.spec.js
+++ spec/card.spec.js
@@ -6,27 +6,27 @@ describe("Card", function() {
        rush5 = new Card("Rush", "offense", 5, "rush.jpg"); 
     });
     
-    it('should be a card', function() {
+    it('is a card', function() {
         expect(rush5).toEqual(jasmine.any(Card));    
     })
     
-    it('should show the play type', function() {
+    it('shows the play type', function() {
         expect(rush5.playType).toEqual("Rush");
     })
     
-    it('should show the play side offense/defense', function() {
+    it('shows the play side offense/defense', function() {
         expect(rush5.side).toEqual("offense");
     })
     
-    it('should show the yard value', function() {
+    it('shows the yard value', function() {
         expect(rush5.yards).toEqual(5);
     })
     
-    it('should return a number for yards', function() {
+    it('returns a number for yards', function() {
         expect(rush5.yards).toEqual(jasmine.any(Number));
     })
     
-    it('should have an image link', function() {
+    it('has an image link', function() {
         expect(rush5.imgsrc).toEqual('rush.jpg');
     })
     
@@ -40,15 +40,15 @@ describe("showCard function", function() {
        rush5 = new Card("Rush", "offense", 5, "rush.jpg"); 
     });
     
-    it('should have a showCard function that returns a string', function() {
+    it('has a showCard function that returns a string', function() {
         expect(rush5.showCard()).toEqual(jasmine.any(String));
     })
     
-    it('should return the yards', function() {
+    it('returns the yards', function() {
         expect(rush5.showCard()).toContain("Rush");
     })
     
-    it('should not return the play side', function() {
+    it('doesnt return the play side', function() {
         expect(rush5.showCard()).not.toContain("offense");
     })
 })
